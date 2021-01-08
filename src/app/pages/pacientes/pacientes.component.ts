5717d34c1330a493164dfed871a12f77f45f3081
Ivan Camilo <rivancamilo@gmail.com>
2021-01-08 17:34:52 -0500

    agregamos componente para listar los  pacientes

---
+++ src/app/pages/pacientes/pacientes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pacientes',
+  templateUrl: './pacientes.component.html',
+  styles: [
+  ]
+})
+export class PacientesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
