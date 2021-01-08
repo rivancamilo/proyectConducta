8ada5110dacaafab1c191277e3d971bcb4fdb479
Ivan Camilo <rivancamilo@gmail.com>
2021-01-08 17:35:32 -0500

    agregamos componente para crear los entrenamientos

---
+++ src/app/pages/entrenamientos/entrenamientos.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entrenamientos',
+  templateUrl: './entrenamientos.component.html',
+  styles: [
+  ]
+})
+export class EntrenamientosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
