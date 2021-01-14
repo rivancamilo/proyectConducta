192870df8d232768145910b1cabdda5e5bda8395
Ivan Camilo <rivancamilo@gmail.com>
2021-01-13 21:44:01 -0500

    creamos una interface y terminamos la configuracion del reactiveForm

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,16 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styles: [
-  ]
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styles: [
+	]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	loginUser: FormGroup;
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private router: Router,
+		private fb:FormBuilder,
+		private userService: UsuariosService
+	) {
+		this.crearFormulario();
+	}
+
+	ngOnInit(): void {
+	}
+
+	crearFormulario() {
+		this.loginUser = this.fb.group({
+			userEmail:[ localStorage.getItem('email') || 'fernanda@email.com', [ Validators.required ]],
+			userPassword:['123456', [ Validators.required ]],
+			userSaveDatos:[  false ]
+		})
+	}
+
+	iniciarSesion(){
+
+		console.log(this.loginUser.value)
+		this.userService.login(this.loginUser.value).subscribe(res => {
+
+			if(this.loginUser.get('userSaveDatos').value){
+				localStorage.setItem('email',this.loginUser.get('userEmail').value)
+			}else{
+				localStorage.removeItem('email')
+			}
+			
+			this.router.navigateByUrl('/');
+
+		},err  =>{
+
+			console.log(err)
+		})
+
+	}
+
+	recoverPass() {
+		this.router.navigateByUrl('recoverpass');
+	}
 
 }
---
+++ src/app/interfaces/login-form.interface.ts
@@ -0,0 +1,5 @@
+export interface loginForm{
+    userEmail:string;
+    userPassword:string;
+    userSaveDatos:boolean;
+}
