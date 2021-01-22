0c0c4c0957f10292daa254885b884f41265c4dc0
Ivan Camilo <rivancamilo@gmail.com>
2021-01-22 17:51:54 -0500

    agregamos metodos para actualizar los datos y unimos el servicio para actualizar el avatar

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -1,6 +1,13 @@
+import { from } from "rxjs"
+import { environment } from "src/environments/environment"
+
+
+const base_url = environment.base_url;
+
 export class Usuario {
 
     constructor(
+        
         public userNombres : string,
         public userApellidos : string,
         public userEmail : string,
@@ -10,9 +17,21 @@ export class Usuario {
         public userContacto : string,
         public userPassword? : string,
         public userSobreMi? : string,
-        public userAvatar? : string
+        public userAvatar? : string,
+        public _id? : string,
     ){
 
     }
 
+    get getImagen(){
+        // http://localhost:3800/api/upload/usuarios/5ffb6522a6303a183c79d969
+        if( this.userAvatar ){
+            return `${base_url}upload/usuarios/${this.userAvatar}`;
+        }else{
+            return `${base_url}upload/usuarios/no-imagen`;
+        }
+    }
+
+
+    
 }
--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Usuario } from 'src/app/models/usuario.model';
+import { FileUploadService } from 'src/app/services/file-upload.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
 	selector: 'app-perfil',
@@ -14,14 +17,32 @@ export class PerfilComponent implements OnInit {
 	public headerTop = document.querySelector('.navbar');
 	public sidebar = document.querySelector('.sidebar');
 	public body = document.querySelector('#colorBody');
+	
+	public usuario:Usuario;
+	public imageSubir: File;
+	public imageTemp:any = '';
+
+
 	perfilUsuario:FormGroup;
 	cambioPassword:FormGroup;  
-	constructor() { }
 
+
+	constructor(private userService:UsuariosService,
+				private fb:FormBuilder,
+				private fileUploadService:FileUploadService) { 
+		this.usuario = this.userService.usuario;
+		this.creaFormPerfil();
+		
+	}
+	
 	ngOnInit(): void {
+		this.datosPerfil();
+
 	}
 
+	/*********************************************************************************
 
+	*********************************************************************************/
 	cambioColorHeader(color: string) {
 		this.header.setAttribute('data-background-color', color);
 	}
@@ -37,10 +58,70 @@ export class PerfilComponent implements OnInit {
 		this.body.setAttribute('data-background-color', color);
 	}
 
+	creaFormPerfil(){
+		this.perfilUsuario = this.fb.group({
+			userNombres:[this.usuario.userNombres ],
+			userApellidos:[ this.usuario.userApellidos ],
+			userEmail:[ this.usuario.userEmail ],
+			userContacto:[ this.usuario.userContacto ],
+			userSobreMi:[ this.usuario.userSobreMi ]
+		})
+	}
+
+	datosPerfil(){
+		this.perfilUsuario.reset({
+			userNombres:this.usuario.userNombres ,
+			userApellidos: this.usuario.userApellidos ,
+			userEmail: this.usuario.userEmail ,
+			userContacto: this.usuario.userContacto ,
+			userSobreMi: this.usuario.userSobreMi 
+		})
+	}
+
 	savePassword(){
 
 	}
 	actualizaPerfil(){
 		
+		if(this.imageSubir){
+			this.subirImagen();
+		}
+
+		this.userService.actualizaUsuario( this.perfilUsuario.value ).subscribe( res => {
+
+			const { userNombres, userApellidos, userContacto, userSobreMi } = res['usuario'];
+			this.usuario.userNombres = userNombres;
+			this.usuario.userApellidos = userApellidos;
+			this.usuario.userContacto =  userContacto;
+			this.usuario.userSobreMi = userSobreMi;
+			
+		},
+		err=>{
+			console.log(err)
+		})
+
 	}
+
+
+
+	cambiarImagen( file: File  ){
+		this.imageSubir = file;
+		if(!file) {
+			return this.imageTemp=null;
+		}
+		const reader = new FileReader();
+		const base64 = reader.readAsDataURL(file);
+		reader.onloadend = () => {
+			this.imageTemp = reader.result;
+			//console.log(reader.result);
+		}
+	}
+
+	subirImagen(){
+		this.fileUploadService.cargarFoto(this.imageSubir,'usuarios',this.usuario._id )
+		.then(img => this.usuario.userAvatar = img )
+	}
+
+
+	
 }
