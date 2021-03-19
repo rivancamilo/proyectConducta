08e58757ea5a59ee26a5438855cf0c3e88975778
Ivan Camilo <rivancamilo@gmail.com>
2021-03-18 22:02:35 -0500

    ocultar menu de usuarios

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -11,9 +11,12 @@ import { UsuariosService } from 'src/app/services/usuarios.service';
 export class SidebarComponent implements OnInit {
 
   public usuario:Usuario;
+  public opUsuario:string;
 
   constructor(private userService:UsuariosService) { 
     this.usuario = userService.usuario;
+    this.opUsuario = this.usuario.userRolID;
+    console.log(this.usuario)
   }
 
   ngOnInit(): void {
